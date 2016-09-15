ca699c9c4a949dcf4919f3314b48e2097ad37475
Pras Gema <prasetyo.gema@gmail.com>
2016-09-15 18:19:36 +0700

    added roles to leads' states

--- public/angular/routes/lead.routes.js
+++ public/angular/routes/lead.routes.js
@@ -17,39 +17,48 @@ angular.module('lead').config(['$stateProvider', '$urlRouterProvider',
   	  })
   	  .state('lead.buyer', {
         url: '/buyer',
-        templateUrl: '/angular/views/lead/buyer.view.html'
+        templateUrl: '/angular/views/lead/buyer.view.html',
+        roles: ['user']
       })
       .state('lead.seller', {
         url: '/seller',
-        templateUrl: '/angular/views/lead/seller.view.html'
+        templateUrl: '/angular/views/lead/seller.view.html',
+        roles: ['user']
       })
       .state('lead.vendor', {
         url: '/vendor',
-        templateUrl: '/angular/views/lead/vendor.view.html'
+        templateUrl: '/angular/views/lead/vendor.view.html',
+        roles: ['user']
       })
       .state('lead.contact', {
         url: '/contact',
-        templateUrl: '/angular/views/lead/contact.view.html'
+        templateUrl: '/angular/views/lead/contact.view.html',
+        roles: ['user']
       })
   	  .state('lead.view-buyer', {
         url: '/buyer/{id}',
-        templateUrl: '/angular/views/lead/view-buyer.view.html'
+        templateUrl: '/angular/views/lead/view-buyer.view.html',
+        roles: ['user']
       })
       .state('lead.view-seller', {
         url: '/seller/{id}',
-        templateUrl: '/angular/views/lead/view-seller.view.html'
+        templateUrl: '/angular/views/lead/view-seller.view.html',
+        roles: ['user']
       })
       .state('lead.view-vendor', {
         url: '/vendor/{id}',
-        templateUrl: '/angular/views/lead/view-vendor.view.html'
+        templateUrl: '/angular/views/lead/view-vendor.view.html',
+        roles: ['user']
       })
       .state('lead.view-contact', {
         url: '/contact/{id}',
-        templateUrl: '/angular/views/lead/view-contact.view.html'
+        templateUrl: '/angular/views/lead/view-contact.view.html',
+        roles: ['user']
       })
       .state('lead.create', {
         url: '/create',
-        templateUrl: '/angular/views/lead/create.view.html'
+        templateUrl: '/angular/views/lead/create.view.html',
+        roles: ['admin']
       });
   }
 ]);
