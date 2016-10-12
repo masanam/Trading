50b62741e05dabbe202ab5a2b78fba2efb793d76
Pras Gema <prasetyo.gema@gmail.com>
2016-10-12 17:27:45 +0700

    ratified concession table

--- database/migrations/2016_09_03_055837_create_consession_table.php
+++ database/migrations/2016_09_03_055837_create_consession_table.php
@@ -16,26 +16,28 @@ class CreateConsessionTable extends Migration
         Schema::create('concession', function (Blueprint $table) {
             $table->increments('id');
             $table->string('concession_name');
-            $table->string('owner');
-            $table->integer('seller_id')->nullable();
-            $table->string('city');
+            $table->integer('seller_id')->unsigned()->nullable();
+            $table->string('owner')->nullable();
             $table->string('address');
+            $table->string('city');
+            $table->string('country');
             $table->decimal('latitude', 10, 8);
             $table->decimal('longitude', 11, 8);
             $table->integer('size');
+            $table->decimal('stripping_ratio', 4, 2);
             $table->integer('resource');
             $table->integer('reserves');
             $table->integer('contracted_volume');
+            $table->integer('remaining_volume');
             $table->integer('annual_production');
-            $table->integer('stockpile_distance');
-            $table->decimal('stripping_ratio', 4, 2);
-            $table->string('road_accessibility');
-            $table->decimal('port_distance', 6, 2);
+            $table->string('hauling_road_name')->nullable(); //nama jalanannya
+            $table->string('road_accessibility'); //mine, province
             $table->integer('road_capacity');
-            $table->string('port_name');
-            $table->string('river_capacity');
+            $table->integer('stockpile_distance');
+            $table->integer('port_id')->nullable();
+            $table->decimal('port_distance', 6, 2);
             $table->string('license_type');
-            $table->date('license_expired_date');
+            $table->date('license_expiry_date');
             $table->char('status', 1); // A = Active , X = Deleted
             $table->timestamps();
         });
