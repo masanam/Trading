798cc5cc4005f9a7a19fc1cad3257b1905c3cc38
Martin S <philipus.martin@rocketmail.com>
2016-10-15 05:26:29 +0700

    sudah bisa filter seperti yang diinginkan

--- public/angular/map/controllers/map.controller.js
+++ public/angular/map/controllers/map.controller.js
@@ -2,7 +2,7 @@
 
 angular.module('map').controller('MapController', ['$scope', '$http', '$stateParams', '$state', 'Map', 'Concession', 'NgMap',
   function($scope, $http, $stateParams, $state, Map, Concession, NgMap) {
-    $scope.filters = [];
+    $scope.filters = [{ field:'gcv_arb', operand: '>=', number: 5000 }];
     $scope.concession = {};
     $scope.concessions = [];
     
@@ -11,17 +11,35 @@ angular.module('map').controller('MapController', ['$scope', '$http', '$statePar
     });
 
     $scope.find = function() {
-      for(var i = 0; i < $scope.filters.length; i++){
-      }
+      
+      $scope.filters_gt = [];
       $scope.filters_lt = [];
       $scope.filters_bet = [];
-      $scope.filters_gt = ['gcv_arb, 5000', 'gcv_adb, 3000'];
+      
+      var temp_filter = [];
+      
+      for(var i = 0; i < $scope.filters.length; i++){
+        if($scope.filters[i].operand === '>='){
+          temp_filter = $scope.filters_gt;
+        }
+        else if($scope.filters[i].operand === '<='){
+          temp_filter = $scope.filters_lt;
+        }
+        else{
+          temp_filter = $scope.filters_bet;
+        }
+        temp_filter.push($scope.filters[i].field+','+$scope.filters[i].number);
+      }
+      
       $scope.concessions = Map.query({ 'gt[]': $scope.filters_gt, 'bet[]': $scope.filters_bet, 'lt[]': $scope.filters_lt });
-      //console.log("vm.positions", $scope.concessions);
     };
     
     $scope.addFilter = function(){
-      $scope.filters.push();
+      $scope.filters.push({ field:'', operand: '', number: 0 });
+    };
+    
+    $scope.deleteFilter = function(filter){
+      $scope.filters.splice($scope.filters.indexOf(filter), 1);
     };
 
     $scope.showDetail = function(event, concession) {
