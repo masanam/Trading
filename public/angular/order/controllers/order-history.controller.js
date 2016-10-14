50175ecaf043e33655e8a0d96c4e5a715eaebc36
kamal13 <rakos94@gmail.com>
2016-10-14 17:53:00 +0700

    add order history

---
+++ public/angular/order/controllers/order-history.controller.js
@@ -0,0 +1,45 @@
+'use strict';
+
+angular.module('order').controller('OrderHistoryController', ['$scope', 'Order',
+  function($scope, Order) {
+    
+    $scope.findAvailableBuy = function() {
+      $scope.orders = Order.query({ type: 'buy', action: 'status', order_status: 'a' });
+    };
+
+    $scope.findFinishedBuy = function() {
+      $scope.orders = Order.query({ type: 'buy', action: 'status', order_status: 'f' });
+    };
+
+    $scope.findCancelledBuy = function() {
+      $scope.orders = Order.query({ type: 'buy', action: 'status', order_status: 'x' });
+    };
+
+    $scope.findAvailableSell = function() {
+      $scope.orders = Order.query({ type: 'sell', action: 'status', order_status: 'a' });
+    };
+
+    $scope.findFinishedSell = function() {
+      $scope.orders = Order.query({ type: 'sell', action: 'status', order_status: 'f' });
+    };
+
+    $scope.findCancelledSell = function() {
+      $scope.orders = Order.query({ type: 'sell', action: 'status', order_status: 'x' });
+    };
+
+    $scope.changeOrderStatusA = function(buy_order) {
+      $scope.buy_orders.splice($scope.buy_orders.indexOf(buy_order), 1);
+      $scope.buy_order = Order.get({ type: 'buy', id: buy_order.id, action: 'changeOrderStatus', order_status: 'a' });
+    };
+
+    $scope.changeOrderStatusF = function(buy_order) {
+      $scope.buy_orders.splice($scope.buy_orders.indexOf(buy_order), 1);
+      $scope.buy_order = Order.get({ type: 'buy', id: buy_order.id, action: 'changeOrderStatus', order_status: 'f' });
+    };
+
+    $scope.changeOrderStatusC = function(buy_order) {
+      $scope.buy_orders.splice($scope.buy_orders.indexOf(buy_order), 1);
+      $scope.buy_order = Order.get({ type: 'buy', id: buy_order.id, action: 'changeOrderStatus', order_status: 'x' });
+    };
+  }
+]);
