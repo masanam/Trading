27c139921face916ce2dacd2dc865fc5c681054b
Pras Gema <prasetyo.gema@gmail.com>
2016-10-20 19:14:23 +0700

    editted migrations to have nullable values

--- database/migrations/2016_09_03_055837_create_concession_table.php
+++ database/migrations/2016_09_03_055837_create_concession_table.php
@@ -18,26 +18,26 @@ class CreateConcessionTable extends Migration
             $table->string('concession_name');
             $table->integer('seller_id')->unsigned()->nullable();
             $table->string('owner')->nullable();
-            $table->string('address');
-            $table->string('city');
-            $table->string('country');
-            $table->decimal('latitude', 10, 8);
-            $table->decimal('longitude', 11, 8);
-            $table->longText('polygon');
+            $table->string('address')->nullable();
+            $table->string('city')->nullable();
+            $table->string('country')->nullable();
+            $table->decimal('latitude', 10, 8)->nullable();
+            $table->decimal('longitude', 11, 8)->nullable();
+            $table->longText('polygon')->nullable();
             $table->integer('size')->nullable();
             $table->decimal('stripping_ratio', 4, 2)->nullable();
             $table->integer('resource')->nullable();
             $table->integer('reserves')->nullable();
             $table->integer('contracted_volume')->nullable();
             $table->integer('remaining_volume')->nullable();
-            $table->integer('annual_production');
+            $table->integer('annual_production')->nullable();
             $table->string('hauling_road_name')->nullable(); //nama jalanannya
-            $table->integer('stockpile_capacity'); //kapasitas 
-            $table->integer('stockpile_coverage');
-            $table->integer('stockpile_distance');
+            $table->integer('stockpile_capacity')->nullable(); //kapasitas 
+            $table->integer('stockpile_coverage')->nullable();
+            $table->integer('stockpile_distance')->nullable();
             $table->integer('port_id')->nullable();
-            $table->decimal('port_distance', 6, 2);
-            $table->string('license_type');
+            $table->decimal('port_distance', 6, 2)->nullable();
+            $table->string('license_type')->nullable();
             $table->date('license_expiry_date')->nullable();
             $table->char('status', 1); // A = Active , X = Deleted
             $table->timestamps();
