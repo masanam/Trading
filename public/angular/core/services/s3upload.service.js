f986db7576f71b6fdefbebbf115216aecbe1b402
Pras Gema <prasetyo.gema@gmail.com>
2016-11-01 12:05:53 +0700

    repaired bug in uploading files

--- public/angular/core/services/s3upload.service.js
+++ public/angular/core/services/s3upload.service.js
@@ -4,10 +4,11 @@ angular.module('index').service('S3Upload', ['$http', 'Upload',
   function ($http, Upload) {
     var self = this;
 
-    this.signing = function (filename, folder, callback){
+    this.signing = function (filename, folder, type, callback){
       var request = {
         filename: filename,
-        folder: folder
+        folder: folder,
+        type: type
       };
 
       //get signing from server side
@@ -21,17 +22,16 @@ angular.module('index').service('S3Upload', ['$http', 'Upload',
     this.send = function (signing, files, callback){
       Upload.upload({
         url: signing.url, //s3Url
+        fields: signing.fields, //credentials
         transformRequest: function(data, headersGetter) {
           var headers = headersGetter();
           delete headers.Authorization;
           return data;
         },
-        fields: signing.fields, //credentials
+        skipAuthorization: true,
         method: 'POST',
         file: files
       }).success(function(data, status, headers, config) {
-        console.log('data', data);
-        console.log('config', config);
         // file is uploaded successfully, return no error
         return callback(undefined, data, config);
       }).error(function(err) {
@@ -43,7 +43,7 @@ angular.module('index').service('S3Upload', ['$http', 'Upload',
     //upload = sign, then send
     this.upload = function (files, filename, folder, callback){
       //sign file before uploading
-      this.signing(filename, folder, function(err, signing){
+      this.signing(filename, folder, files.type, function(err, signing){
         //return error message if failed to get signing
         if(err) return callback(err);
 
