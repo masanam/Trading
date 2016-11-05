e5f071b037b9602a8609829f6f356ae4b5dea982
Pras Gema <prasetyo.gema@gmail.com>
2016-11-05 19:40:02 +0700

    ratified frontend for orders

---
+++ public/angular/order/controllers/order/order.controller.js
@@ -0,0 +1,8 @@
+'use strict';
+
+angular.module('order').controller('OrderController', ['$scope', '$stateParams', '$state', 'Order',
+  function($scope, $stateParams, $state, Order) {
+      
+  }
+]);
+
--- public/angular/order/services/order.service.js
+++ public/angular/order/services/order.service.js
@@ -2,18 +2,10 @@
 
 angular.module('order').factory('Order', ['$resource',
   function ($resource) {
-    return $resource('api/order/:option/:type/:id/:action/:order_status/:progress_status/:sellerId', {
-      option:undefined,
-      type:undefined,
+    return $resource('api/order/:id', {
       id: undefined,
-      action: undefined,
-      order_status: undefined,
-      progress_status: undefined,
-      sellerId:undefined
     }, {
-      update: {
-        method: 'PUT'
-      }
+      update: { method: 'PUT' }
     });
   }
 ]);
