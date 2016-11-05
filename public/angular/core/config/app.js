f95e3ae2a8b9372f8d980f20f23a444e4a2602cb
Pras Gema <prasetyo.gema@gmail.com>
2016-11-06 00:48:10 +0700

    added date http interceptor

--- public/angular/core/config/app.js
+++ public/angular/core/config/app.js
@@ -33,6 +33,30 @@ angular.module(ApplicationConfiguration.applicationModuleName).config(['$locatio
     $locationProvider.html5Mode(true).hashPrefix('!');
 
     //$httpProvider.interceptors.push('authInterceptor');
+    var dateRegex = /\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}/;
+    var convertDateStringsToDates = function (input) {
+      for (var key in input) {
+        if (!input.hasOwnProperty(key)) continue;
+
+        var value = input[key];
+        var match;
+        // Check for string properties which look like dates.
+        if (typeof value === "string" && (match = value.match(dateRegex))) {
+          var milliseconds = Date.parse(match[0])
+          if (!isNaN(milliseconds)) {
+            input[key] = new Date(milliseconds);
+          }
+        } else if (typeof value === "object") {
+            // Recurse into object
+          convertDateStringsToDates(value);
+        }
+      }
+    }
+
+    $httpProvider.defaults.transformResponse.push(function(input){
+      convertDateStringsToDates(input);
+      return input;
+    });
   }
 ]);
 
