dc62f91731d777fac5f36d431a49b25a5a561673
Pras Gema <prasetyo.gema@gmail.com>
2016-11-09 15:03:37 +0700

    divided Orders in dashboard than in main ones

--- public/angular/dashboard/controllers/main-dashboard.controller.js
+++ public/angular/dashboard/controllers/main-dashboard.controller.js
@@ -1,19 +1,11 @@
 'use strict';
 
-angular.module('dashboard').controller('MainDashboardController', ['$scope', 'Index','Order','Authentication',
-  function($scope, Index, Order, Authentication) {
+angular.module('dashboard').controller('MainDashboardController', ['$scope', 'Index',
+  function($scope, Index) {
     $scope.getIndices = function () {
       $scope.date = new Date();
       $scope.indexPage = 0;
       $scope.indices = Index.query({ action: 'single-date' });
     };
-
-    //find list of order in dashboard
-    $scope.findDashboard = function () {
-      if(Authentication.user.role == 'trader'){
-        $scope.orders = Order.query({ possession: 'my', status: 'p' });
-      }
-    };
-
   }
 ]);
