ff4003c7977605d94e505d3226f540d2a13b1022
Andez NET <andeztea@gmail.com>
2016-11-09 18:28:11 +0700

    order list in dashboard: manager & trader version

--- public/angular/dashboard/controllers/order-dashboard.controller.js
+++ public/angular/dashboard/controllers/order-dashboard.controller.js
@@ -6,7 +6,14 @@ angular.module('dashboard').controller('OrderDashboardController', ['$scope', 'I
 
     //find list of order in dashboard
     $scope.find = function () {
-      $scope.orders = Order.query({ possession: 'my', status: 'p' });
+    	if( Authentication.user.role === 'manager'){
+    		$scope.orders = Order.query({ possession: 'subordinates', status: 'p' });
+    	}
+
+    	if( Authentication.user.role === 'trader'){
+			$scope.orders = Order.query({ possession: 'my', status: 'p' });
+    	}
+      
     };
   }
 ]);
