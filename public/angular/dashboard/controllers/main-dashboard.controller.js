36b032884a0817c8351c9a49f97898fe80c7bdc8
Andez NET <andeztea@gmail.com>
2016-11-09 13:44:42 +0700

    order in dasboard

--- public/angular/dashboard/controllers/main-dashboard.controller.js
+++ public/angular/dashboard/controllers/main-dashboard.controller.js
@@ -1,11 +1,19 @@
 'use strict';
 
-angular.module('dashboard').controller('MainDashboardController', ['$scope', 'Index',
-  function($scope, Index) {
+angular.module('dashboard').controller('MainDashboardController', ['$scope', 'Index','Order','Authentication',
+  function($scope, Index, Order, Authentication) {
     $scope.getIndices = function () {
       $scope.date = new Date();
       $scope.indexPage = 0;
       $scope.indices = Index.query({ action: 'single-date' });
     };
+
+    //find list of order in dashboard
+    $scope.findDashboard = function () {
+      if(Authentication.user.role == 'trader'){
+        $scope.orders = Order.query({ possession: 'my', status: 'p' });
+      }
+    };
+
   }
 ]);
