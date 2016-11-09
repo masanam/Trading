b2e17970328582c37b1a38be61c1fd9050ea5c95
Pras Gema <prasetyo.gema@gmail.com>
2016-11-10 00:46:40 +0700

    ratified all models and relations

--- app/Model/Order.php
+++ app/Model/Order.php
@@ -12,25 +12,25 @@ class Order extends Model
     protected $table = 'orders';
 
     public function buys()
-		{
+    {
       return $this->morphedByMany(BuyOrder::class, 'orderable', 'order_details')->withPivot('id', 'price', 'volume');;
-		}
+    }
 
     public function sells()
-		{
+    {
       return $this->morphedByMany(SellOrder::class, 'orderable', 'order_details')->withPivot('id', 'price', 'volume');;
-		}
+    }
 
-		public function approvals()
-		{
-			return $this->belongsToMany(User::class, 'order_approvals')->withPivot('status', 'updated_at');
-		}
+    public function approvals()
+    {
+      return $this->belongsToMany(User::class, 'order_approvals')->withPivot('status', 'updated_at');
+    }
 
     public function trader() {
-    	return $this->belongsTo(User::class, 'user_id');
+      return $this->belongsTo(User::class, 'user_id');
     }
 
     public function users() {
-    	return $this->belongsToMany(User::class, 'order_users')->withPivot('role');
+      return $this->belongsToMany(User::class, 'order_users')->withPivot('role');
     }
 }
--- app/Model/Port.php
+++ app/Model/Port.php
@@ -7,19 +7,27 @@ use App\Model\Seller;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Port extends Model
-{
-    protected $table = 'ports';
+class Port extends Model {
+  protected $table = 'ports';
 
-    public function Concession() {
-    	return $this->hasMany('App\Model\Concession');
-    }
+  public function Concession() {
+    return $this->hasMany('App\Model\Concession');
+  }
 
-    public function BuyerPort() {
-    	return $this->belongsTo('App\Model\BuyerPort');
-    }
+  public function BuyerPort() {
+    return $this->belongsTo('App\Model\BuyerPort');
+  }
 
-    public function SellerPort() {
-    	return $this->belongsTo('App\Model\SellerPort');
-    }
+  public function SellerPort() {
+      return $this->belongsTo('App\Model\SellerPort');
+  }
+
+  public function buyers() {
+    return $this->belongsToMany(Buyer::class, 'buyer_port');
+  }
+
+  public function sellers() {
+    return $this->belongsToMany(Seller::class, 'port_seller');
+  }
+
 }
