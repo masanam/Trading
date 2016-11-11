bc2f219908588cba60a527e81d9ee815f09636bc
kamal13 <rakos94@gmail.com>
2016-11-11 15:30:32 +0700

    fixing lead all

--- app/Http/Controllers/BuyOrderController.php
+++ app/Http/Controllers/BuyOrderController.php
@@ -27,8 +27,8 @@ class BuyOrderController extends Controller
      */
     public function index()
     {
-
-        $buy_order = BuyOrder::with('Buyer','User','trader')->where('order_status', 1)->orwhere('order_status', 2)->orwhere('order_status', 3)->orwhere('order_status', 4)->orwhere('order_status', 'v')->orwhere('order_status', 'l')->orwhere('order_status', 's')->orwhere('order_status', 'p')->get();
+        $user_id = Auth::User()->id;
+        $buy_order = BuyOrder::with('Buyer','User','trader')->where([['order_status', '1'], ['user_id', $user_id],])->orwhere([['order_status', '2'], ['user_id', $user_id],])->orwhere([['order_status', '3'], ['user_id', $user_id],])->orwhere([['order_status', '4'], ['user_id', $user_id],])->orwhere('order_status', 'v')->orwhere('order_status', 'l')->orwhere('order_status', 's')->orwhere('order_status', 'p')->get();
         return response()->json($buy_order, 200);
     }
 
