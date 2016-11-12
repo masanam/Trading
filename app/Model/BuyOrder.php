27b3d79b8f4c408a1f75a7692278c1fa34b664e3
kamal13 <rakos94@gmail.com>
2016-11-12 20:06:25 +0700

    fix available volume

--- app/Model/BuyOrder.php
+++ app/Model/BuyOrder.php
@@ -37,6 +37,6 @@ class BuyOrder extends Model
     }
 
     public function OrderDetail() {
-        return $this->belongsTo('App\Model\OrderDetail', 'id', 'orderable_id')->where('orderable_type','App\Model\BuyOrder');
+        return $this->hasMany('App\Model\OrderDetail', 'orderable_id', 'id')->where('orderable_type','App\Model\BuyOrder');
     }
 }
