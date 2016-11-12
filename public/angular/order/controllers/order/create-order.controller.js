1c161b058e0a56b02ba76255a80bdec4b3a68a4e
Pras Gema <prasetyo.gema@gmail.com>
2016-11-12 22:10:56 +0700

    indentation error

--- public/angular/order/controllers/order/create-order.controller.js
+++ public/angular/order/controllers/order/create-order.controller.js
@@ -2,11 +2,11 @@
 
 angular.module('order').controller('CreateOrderController', ['$scope', '$state', 'Order',
   function($scope, $state, Order) {
-  	$scope.init = function (){
-	    $scope.order = new Order();
-	    $scope.order.buys = $scope.order.sells = [];
-	    console.log('create order', $scope.order);
-	  };
+    $scope.init = function (){
+      $scope.order = new Order();
+      $scope.order.buys = $scope.order.sells = [];
+      console.log('create order', $scope.order);
+    };
 
     $scope.submit = function () {
       console.log($scope.order);
