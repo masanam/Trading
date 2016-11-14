236937b86c3798d4c5d2cb9439e6bbafa0d9d42a
Andez Net <andeztea@gmail.com>
2016-11-14 16:17:47 +0700

    operation

---
+++ public/angular/operation/controllers/operation.controller.js
@@ -0,0 +1,7 @@
+'use strict';
+
+angular.module('operation').controller('OperationController', ['$scope', '$stateParams', 'Order', 'OrderUser', 'Authentication', '$location', 'Chat',
+	function($scope, $stateParams, Order, OrderUser, Authentication, $location, Chat) {
+ 
+
+}]);
---
+++ public/angular/operation/services/operation.service.js
@@ -0,0 +1,43 @@
+'use strict';
+
+angular.module('chat').factory('Chat', ['Authentication', 'FirebaseService', '$firebaseArray',
+  function (Authentication, FirebaseService, $firebaseArray) {
+    var sell_chats = [];
+    var mainApp = FirebaseService.mainApp;
+
+    return {
+      findChatByOrder: function(orderId, callback) {
+        var path_chat = 'order_chat/' + orderId;
+        var ref = mainApp.database().ref(path_chat);
+        var chats = $firebaseArray(ref);
+        return callback(chats);
+      },
+
+      sendChat: function(orderId, userId, message) {
+        // sending chat
+        var chat = {
+          'order_id': orderId,
+          'user_id': Authentication.user.id,
+          'author': Authentication.user.name,
+          'message': message,
+          'created_at': Date.now()
+        };
+        //console.log(chat);
+        
+        var chat_key = mainApp.database().ref('order_chat/' + orderId).push(chat).key;
+
+        // sending notification
+        var notification = {
+          'url': 'order/' + orderId,
+          'notification': Authentication.user.name + ' : "' + message + '"',
+          'created_at': Date.now(),
+          'isRead': false
+        };
+
+        for (var i = userId.length - 1; i >= 0; i--) {
+          var notif_key = mainApp.database().ref('notification/' + userId[i].user_id).push(notification).key;
+        }
+      }
+    };
+  }
+]);
