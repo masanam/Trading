604dd06d73a50437a6f94ff8968c77322fd4a31a
kamal13 <rakos94@gmail.com>
2016-11-22 16:40:31 +0700

    add additional cost

--- public/angular/order/controllers/order/add-cost.modal.controller.js
+++ public/angular/order/controllers/order/add-cost.modal.controller.js
@@ -2,10 +2,22 @@
 
 angular.module('order').controller('AddCostModalController', ['$uibModalInstance', '$scope', 'Order', 'Notification',
   function($uibModalInstance, $scope, Order, Notification) {
-    $scope.order = new Order($scope.order);
+
+    if ($scope.order.additional === 'buy') {
+      $scope.additional = new Order($scope.order.sells[$scope.order.index].additional);
+    }else if ($scope.order.additional === 'sell') {
+      $scope.additional = new Order($scope.order.buys[$scope.order.index].additional);
+      
+    }
 
     $scope.ok = function () {
-      $uibModalInstance.close($scope.order);
+      if ($scope.order.additional === 'buy') {
+        $scope.order.sells[$scope.order.index].additional = $scope.additional;
+        $uibModalInstance.close($scope.order.sells[$scope.order.index].additional);
+      }else if ($scope.order.additional === 'sell') {
+        $scope.order.buys[$scope.order.index].additional = $scope.additional;
+        $uibModalInstance.close($scope.order.buys[$scope.order.index].additional);
+      }
     };
 
     $scope.cancel = function () {
