dc6c328a6baa495048f0cfc774040609d838946c
kamal13 <rakos94@gmail.com>
2016-12-13 15:14:50 +0700

    simplying lead controller

--- app/Http/Controllers/LeadController.php
+++ app/Http/Controllers/LeadController.php
@@ -181,11 +181,10 @@ class LeadController extends Controller
                 'message' => 'Bad Request'
             ], 400);
         }
-        return response()->json($req, 200);
 
         $lead = Lead::find($id);
         $lead->user_id = Auth::User()->id;
-        $lead->company_id = $req->company_id;
+        $lead->fill($req->all());
 
         if ($req->lead_type === 'buy') {
             $lead->lead_type = 'b';
@@ -199,88 +198,7 @@ class LeadController extends Controller
         $lead->laycan_start = date('Y-m-d',strtotime($req->laycan_start));
         $lead->laycan_end = date('Y-m-d',strtotime($req->laycan_end));
 
-        $lead->factory_id = $req->factory_id;
-        $lead->address = $req->address;
-        $lead->city = $req->city;
-        $lead->country = $req->country;
-        $lead->latitude = $req->latitude;
-        $lead->longitude = $req->longitude;
-        $lead->port_distance = $req->port_distance;
-        $lead->port_id = $req->port_id;
-        $lead->port_name = $req->port_name;
-        $lead->port_status = $req->port_status;
-        $lead->port_daily_rate = $req->port_daily_rate;
-        $lead->port_draft_height = $req->port_draft_height;
-        $lead->port_latitude = $req->port_latitude;
-        $lead->port_longitude = $req->port_longitude;
-
-        $lead->product_name = $req->product_name;
-        $lead->typical_quality = $req->typical_quality;
-        $lead->product_id = $req->product_id;
-
-        $lead->gcv_arb_min = $req->gcv_arb_min;
-        $lead->gcv_arb_max = $req->gcv_arb_max;
-        $lead->gcv_arb_reject = $req->gcv_arb_reject;
-        $lead->gcv_arb_bonus = $req->gcv_arb_bonus;
-        $lead->gcv_adb_min = $req->gcv_adb_min;
-        $lead->gcv_adb_max = $req->gcv_adb_max;
-        $lead->gcv_adb_reject = $req->gcv_adb_reject;
-        $lead->gcv_adb_bonus = $req->gcv_adb_bonus;
-        $lead->ncv_min = $req->ncv_min;
-        $lead->ncv_max = $req->ncv_max;
-        $lead->ncv_reject = $req->ncv_reject;
-        $lead->ncv_bonus = $req->ncv_bonus;
-        $lead->ash_min = $req->ash_min;
-        $lead->ash_max = $req->ash_max;
-        $lead->ash_reject = $req->ash_reject;
-        $lead->ash_bonus = $req->ash_bonus;
-        $lead->ts_min = $req->ts_min;
-        $lead->ts_max = $req->ts_max;
-        $lead->ts_reject = $req->ts_reject;
-        $lead->ts_bonus = $req->ts_bonus;
-        $lead->tm_min = $req->tm_min;
-        $lead->tm_max = $req->tm_max;
-        $lead->tm_reject = $req->tm_reject;
-        $lead->tm_bonus = $req->tm_bonus;
-        $lead->im_min = $req->im_min;
-        $lead->im_max = $req->im_max;
-        $lead->im_reject = $req->im_reject;
-        $lead->im_bonus = $req->im_bonus;
-        $lead->fc_min = $req->fc_min;
-        $lead->fc_max = $req->fc_max;
-        $lead->fc_reject = $req->fc_reject;
-        $lead->fc_bonus = $req->fc_bonus;
-        $lead->vm_min = $req->vm_min;
-        $lead->vm_max = $req->vm_max;
-        $lead->vm_reject = $req->vm_reject;
-        $lead->vm_bonus = $req->vm_bonus;
-        $lead->hgi_min = $req->hgi_min;
-        $lead->hgi_max = $req->hgi_max;
-        $lead->hgi_reject = $req->hgi_reject;
-        $lead->hgi_bonus = $req->hgi_bonus;
-        $lead->size_min = $req->size_min;
-        $lead->size_max = $req->size_max;
-        $lead->size_reject = $req->size_reject;
-        $lead->size_bonus = $req->size_bonus;
-        $lead->fe2o3_min = $req->fe2o3_min;
-        $lead->fe2o3_max = $req->fe2o3_max;
-        $lead->fe2o3_reject = $req->fe2o3_reject;
-        $lead->fe2o3_bonus = $req->fe2o3_bonus;
-        $lead->aft_min = $req->aft_min;
-        $lead->aft_max = $req->aft_max;
-        $lead->aft_reject = $req->aft_reject;
-        $lead->aft_bonus = $req->aft_bonus;
-
-        $lead->volume = $req->volume;
-        $lead->price = $req->price;
-        $lead->trading_term = $req->trading_term;
-        $lead->trading_term_detail = $req->trading_term_detail;
-        $lead->payment_terms = $req->payment_terms;
-        $lead->commercial_term = $req->commercial_term;
-        $lead->penalty_desc = $req->penalty_desc;
-
         $lead->order_status = $req->order_status;
-        $lead->progress_status = $req->progress_status;
 
         $lead->save();
 
