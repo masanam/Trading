b145db3d9c3b1b5947cd28d234f9495ba3ff50b3
kamal13 <rakos94@gmail.com>
2016-12-15 12:41:24 +0700

    adjustmen

--- public/angular/lead/controllers/lead.controller.js
+++ public/angular/lead/controllers/lead.controller.js
@@ -2,9 +2,9 @@
 
 angular.module('lead').controller('LeadController', ['$scope', '$stateParams', 'Authentication', 'Order', 'Lead', 'Term',
   function($scope, $stateParams, Authentication, Order, Lead, Term) {
-  	$scope.Authentication = Authentication;
-  	$scope.tradingTerm = Term.trading;
-  	$scope.paymentTerm = Term.payment;
+    $scope.Authentication = Authentication;
+    $scope.tradingTerm = Term.trading;
+    $scope.paymentTerm = Term.payment;
 
     $scope.findOne = function(id){
       if(!id) id = $stateParams.id;
@@ -12,22 +12,22 @@ angular.module('lead').controller('LeadController', ['$scope', '$stateParams', '
     };
 
     $scope.find = function(status) {
-    	if(!status) status = $stateParams.status;
+      if(!status) status = $stateParams.status;
       $scope.leads = Lead.query({ status: status });
     };
 
     $scope.findRecommendations = function () {
-    	$scope.leadRecommendations = Lead.query({ matching: 'leads', lead_id: $stateParams.id });
-    	$scope.productRecommendations = Lead.query({ matching: 'products', lead_id: $stateParams.id });
+      $scope.leadRecommendations = Lead.query({ matching: 'leads', lead_id: $stateParams.id });
+      $scope.productRecommendations = Lead.query({ matching: 'products', lead_id: $stateParams.id });
     };
 
     $scope.init = function () {
-    	$scope.lead = new Lead();
+      $scope.lead = new Lead();
       if($stateParams.lead_type) $scope.lead.lead_type = $stateParams.lead_type;
     };
 
     $scope.nextToOperation = function(lead){
-      var lead = new Lead(lead);
+      lead = new Lead(lead);
 
       lead.$save(function(res) {
         console.log(res);
