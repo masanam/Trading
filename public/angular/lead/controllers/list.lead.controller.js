baaaa984af3298bca62c1570119eb458b65fe8df
kamal13 <rakos94@gmail.com>
2016-12-20 15:05:47 +0700

    fix bug lead

--- public/angular/lead/controllers/list.lead.controller.js
+++ public/angular/lead/controllers/list.lead.controller.js
@@ -19,29 +19,17 @@ angular.module('lead').controller('ListLeadController', ['$scope', '$location', 
       $scope.leads = Lead.query({ lead_type: $lead_type, order_status: $order_status });
     };
 
-    $scope.toSupplier = function(id) {
-      $location.path('sell-order/create/supplier/'+id);
-    };
-
-    $scope.toConcession = function(company_id,id) {
-      $location.path(id+'/concession/'+company_id);
-    };
-
-    $scope.toProduct = function(seller_id,id,concession_id) {
-      $location.path('sell-order/create/product/'+seller_id+'/'+id+'/'+concession_id);
-    };
-
-    $scope.toPort = function(seller_id,id,concession_id) {
-      $location.path('sell-order/create/port/'+seller_id+'/'+id+'/'+concession_id);
-    };
-
-    $scope.toSummary = function(seller_id,id,concession_id) {
-      $location.path('sell-order/create/summary/'+seller_id+'/'+id+'/'+concession_id);
-    };
-
     $scope.changeOrderStatus = function(sell_order, order_status) {
       $scope.sell_orders.splice($scope.sell_orders.indexOf(sell_order), 1);
       $scope.sell_order = Order.get({ type: 'sell', id: sell_order.id, action: 'changeOrderStatus', order_status: order_status });
+    };
+
+    $scope.status = function (lead, status) {
+      lead.order_status = status;
+
+      lead.$update(function(res){
+        $scope.leads.splice($scope.leads.indexOf(lead), 1);
+      });
     };
 
   }
