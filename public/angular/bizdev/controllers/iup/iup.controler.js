57b813d5d8dc2244e2cc377d2de3d2de4c5130d3
myrtsws <myrtylly@gmail.com>
2016-12-20 18:53:44 +0700

    approval dan declined reason modal done

--- public/angular/bizdev/controllers/iup/iup.controler.js
+++ public/angular/bizdev/controllers/iup/iup.controler.js
@@ -19,7 +19,29 @@ angular.module('bizdev').controller('IupController', ['$scope', '$stateParams', 
         animation: true,
         ariaLabelledBy: 'modal-title',
         ariaDescribedBy: 'modal-body',
-        templateUrl: './angular/bizdev/views/map/create.view.html',
+        templateUrl: './angular/bizdev/views/map/create.modal.html',
+        windowClass: 'xl-modal',
+        controller: 'IupModalController'
+      });
+    };
+
+    $scope.approved = function () {
+      var modalInstance = $uibModal.open({
+        animation: true,
+        ariaLabelledBy: 'modal-title',
+        ariaDescribedBy: 'modal-body',
+        templateUrl: './angular/bizdev/views/iup/approved.modal.html',
+        windowClass: 'xl-modal',
+        controller: 'IupModalController'
+      });
+    };
+
+    $scope.declined = function () {
+      var modalInstance = $uibModal.open({
+        animation: true,
+        ariaLabelledBy: 'modal-title',
+        ariaDescribedBy: 'modal-body',
+        templateUrl: './angular/bizdev/views/iup/declined.modal.html',
         windowClass: 'xl-modal',
         controller: 'IupModalController'
       });
