77de10270de55d139bc669e67cfc352af24d3a0d
Aryo Pradipta Gema <aryopg@gmail.com>
2016-12-23 16:56:31 +0700

    fix coalpedia map

--- public/angular/coaltradephase2/controllers/sales/sales.controller.js
+++ public/angular/coaltradephase2/controllers/sales/sales.controller.js
@@ -3,9 +3,8 @@
 angular.module('sales').controller('SalesController', ['$scope', '$stateParams', '$state', 
   function($scope, $stateParams, $state) {
     $scope.IsHidden = true;
-      $scope.ShowHide = function () {
-        $scope.IsHidden = $scope.IsHidden ? false : true;
-      };
-    
+    $scope.ShowHide = function () {
+      $scope.IsHidden = $scope.IsHidden ? false : true;
+    };
   }
 ]);
