ebf99ab0b5a4386c1f15cca91fe6fb03b06c2f36
Andez NET <andeztea@gmail.com>
2016-12-23 19:00:48 +0700

    sales coaltrade phase 2

--- public/angular/coaltradephase2/controllers/sales/sales.controller.js
+++ public/angular/coaltradephase2/controllers/sales/sales.controller.js
@@ -1,18 +1,36 @@
 'use strict';
 
-angular.module('sales').controller('SalesController', ['$scope', '$stateParams', '$state', 
-  function($scope, $stateParams, $state) {
+angular.module('sales').controller('SalesController', ['$scope', '$stateParams', '$state','$uibModal', 
+  function($scope, $stateParams, $state, $uibModal) {
 
-    $scope.detail = [
-        { ContractID:'CONTRACT0010',Costumer:'PT.ABC',Product:'',Tonnage:'30.000.000mt',Price:'$54',Planed:'MUST BE FILLED!' },
-        { ContractID:'CONTRACT0011',Costumer:'PT.ABCD',Product:'',Tonnage:'40.000.000mt',Price:'$64',Planed:'MUST BE FILLED!' },   
-        { ContractID:'CONTRACT0012',Costumer:'PT.ABCDE',Product:'',Tonnage:'42.000.000mt',Price:'$74',Planed:'MUST BE FILLED!' }    
-    ]; 
-
-    $scope.click=function(ContractID){
-      $scope.contract=$scope.detail[ContractID];
-
+    $scope.opendetailModal = function () {
+      var modalInstance = $uibModal.open({
+        windowClass: 'xl-modal',
+        templateUrl: './angular/coaltradephase2/views/sales/modal.view.detail.html',
+        controller: 'SalesModalController',
+        scope: $scope,
+      });
     };
+
+    $scope.openaddveselModal = function () {
+      var modalInstance = $uibModal.open({
+        windowClass: 'xl-modal',
+        templateUrl: './angular/coaltradephase2/views/sales/add_vesel.html',
+        controller: 'SalesModalController',
+        scope: $scope,
+      });
+    };
+
+    //$scope.detail = [
+        //{ ContractID:'CONTRACT0010',Costumer:'PT.ABC',Product:'',Tonnage:'30.000.000mt',Price:'$54',Planed:'MUST BE FILLED!' },
+        //{ ContractID:'CONTRACT0011',Costumer:'PT.ABCD',Product:'',Tonnage:'40.000.000mt',Price:'$64',Planed:'MUST BE FILLED!' },   
+        //{ ContractID:'CONTRACT0012',Costumer:'PT.ABCDE',Product:'',Tonnage:'42.000.000mt',Price:'$74',Planed:'MUST BE FILLED!' }    
+    //]; 
+
+    //$scope.click=function(ContractID){
+      //$scope.contract=$scope.detail[ContractID];
+
+    //};
 
   }
 ]);
