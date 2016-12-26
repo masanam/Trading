9097d594eabce4c680a973880a3854e758472618
myrtsws <myrtylly@gmail.com>
2016-12-26 16:10:51 +0700

    tresuary di benerin

--- public/angular/coaltradephase2/controllers/treasury/treasury.controller.js
+++ public/angular/coaltradephase2/controllers/treasury/treasury.controller.js
@@ -3,17 +3,36 @@
 angular.module('treasury').controller('TreasuryController', ['$scope', '$stateParams', '$state', 
   function($scope, $stateParams, $state) {
 
-    $scope.labels = ['2011', '2012', '2013', '2014', '2015', '2016'];
-    $scope.data = [
-    	[28, 48, 40, 19, 86, 27, 90]
-    ];
-
     $scope.onClick = function (points, evt) {
       console.log(points, evt);
     };
 
     $scope.datasetOverride = [{ yAxisID: 'y-axis-1' }];
+
+    $scope.labels = ['2011', '2012', '2013', '2014', '2015', '2016'];
+    $scope.data = [
+    	[1738, 1934, 2512, 2621, 2621, 3131]
+    ];
+    
     $scope.options = {
+      scales: {
+        yAxes: [
+          {
+            id: 'y-axis-1',
+            type: 'linear',
+            display: true,
+            position: 'left'
+          }
+        ]
+      }
+    };
+
+    $scope.labels1 = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
+    $scope.data1 = [
+      [239, 123, 120, 348, 323, 345, 234, 156, 346, 234, 345, 364]
+    ];
+    
+    $scope.options1 = {
       scales: {
         yAxes: [
           {
