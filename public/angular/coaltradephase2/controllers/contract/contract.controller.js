e7b19baa741c8461b6db519c63e7e55c7a3154e7
myrtsws <myrtylly@gmail.com>
2016-12-27 11:39:56 +0700

    contract done

--- public/angular/coaltradephase2/controllers/contract/contract.controller.js
+++ public/angular/coaltradephase2/controllers/contract/contract.controller.js
@@ -2,13 +2,7 @@
 
 angular.module('contract').controller('ContractController', ['$scope', '$stateParams', '$state', 
   function($scope, $stateParams, $state) {
-
-    $scope.onClick = function (points, evt) {
-      console.log(points, evt);
-    };
-
-    $scope.datasetOverride = [{ yAxisID: 'y-axis-1' }];
-    
+  	
   }
 ]);
  
