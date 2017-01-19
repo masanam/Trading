9fef5bdef523d33bb60d11919e0524cec47d0442
Aryo Pradipta Gema <aryopg@gmail.com>
2017-01-19 14:32:42 +0700

    latest shipment log and other standard CRUD of shipment log and shipment history

--- app/Model/ShipmentLog.php
+++ app/Model/ShipmentLog.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class ShipmentLog extends Model
 {
+  protected $table = 'shipment_log';
+  
   public function shipments() {
     return $this->belongsTo(Shipment::class, 'shipment_id');
   }
