9e725d2e0c7782020890580262fd858549109e2f
Pras Gema <prasetyo.gema@gmail.com>
2017-01-19 11:35:24 +0700

    ratified db structure myrtyl

--- database/migrations/2017_01_18_125821_create_tonnages_table.php
+++ database/migrations/2017_01_18_125821_create_tonnages_table.php
@@ -13,29 +13,16 @@ class CreateTonnagesTable extends Migration
      */
     public function up()
     {
-        Schema::create('tonnages', function (Blueprint $table) {
+        Schema::create('sales_target', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('product_id')->unsigned();           
             $table->integer('month');
             $table->integer('year');
             $table->integer('value');
-            $table->dateTime('updated_at');
-            $table->dateTime('created_at');
             $table->char('status', 1);
+            $table->timestamps();
 
             $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
-        });
-
-        Schema::create('tonnages_history', function (Blueprint $table) {
-            $table->increments('id');
-            $table->integer('product_id')->unsigned();
-            $table->integer('month');
-            $table->integer('year');
-            $table->integer('value');
-            $table->integer('updated_by')->unsigned();
-            $table->dateTime('updated_at');
-            $table->char('status', 1);
-            $table->dateTime('created_at');            
         });
     }
 
@@ -46,7 +33,6 @@ class CreateTonnagesTable extends Migration
      */
     public function down()
     {
-        Schema::drop('tonnages');
-        Schema::drop('tonnages_history');
+        Schema::drop('sales_target');
     }
 }
