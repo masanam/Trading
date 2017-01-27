fb95c491fcbc043642f5ce585c08f0a0c9561cff
martin s <philipus.martin@rocketmail.com>
2017-01-27 14:50:58 +0700

    testing dump in server

--- app/Mail/ApprovalRequest.php
+++ app/Mail/ApprovalRequest.php
@@ -32,16 +32,13 @@ class ApprovalRequest extends Mailable
      */
     public function build()
     {
-      var_dump($this->order->sells);
-      var_dump($this->order->buys);
-      die;
-        /*return $this->from('noreply@volantech.io')
+        return $this->from('noreply@volantech.io')
             ->view('mail.order.approval')
             ->with([
                 'order' => $this->order,
                 'approval_token' => $this->approval_token,
                 'index_price' => $this->index_price
-            ]);*/
+            ]);
             //->text('mail.order.detail');
     }
 }
