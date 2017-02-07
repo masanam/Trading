961f6fb1479d77ef5353236155e3c04a96807863
Kamal Firdaus <Kamal Firdaus>
2017-02-07 17:11:05 +0700

    quality modul

--- app/Model/QualityDetail.php
+++ app/Model/QualityDetail.php
@@ -6,12 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class QualityDetail extends Model
 {
-    // protected $table = 'quality_details';
+    protected $table = 'quality_details';
+
     // protected $fillable = [
     //   'quality_id', 'value', 'quality'
     // ];
 
     public function quality() {
       return $this->hasMany(Quality::class);
+    }
+
+    public function qualityMetric() {
+      return $this->belongsTo(QualityMetric::class, 'quality_metrics_id','id');
     }
 }
