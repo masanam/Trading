54db29d5e18064b48eb8926320743ef367ac3912
Pras Gema <prasetyo.gema@gmail.com>
2017-02-14 18:30:09 +0700

    condition to CrossLead

--- app/Policies/OrderPolicy.php
+++ app/Policies/OrderPolicy.php
@@ -19,22 +19,39 @@ class OrderPolicy
    */
   public function view(User $user, Order &$order)
   {
-    $subs = $user->subordinates();
-    $users = $subs->pluck('id')->all(); 
-    $users[] = $user->id;
-
     // modify records based on the positions
-    foreach($order->sells as &$sell)
-      if(!in_array($sell->user_id, $users))
-        $sell->seller = $sell->location = $sell->port_name = $sell->address = '-hidden value-';
+    if(config('app.hideCrossingLead')){
+      $subs = $user->subordinates();
+      $users = $subs->pluck('id')->all(); 
+      $users[] = $user->id;
 
-    foreach($order->buys as &$buy)
-      if(!in_array($buy->user_id, $users))
-        $buy->buyer = $buy->location = $buy->port_name = $buy->address = '-hidden value-';
+      foreach($order->sells as &$sell)
+        if(!in_array($sell->user_id, $users))
+          $sell->seller = $sell->location = $sell->port_name = $sell->address = '-hidden value-';
 
-    foreach($order->users as $orderUser){
-      if($orderUser->id === $user->id) return true;
+      foreach($order->buys as &$buy)
+        if(!in_array($buy->user_id, $users))
+          $buy->buyer = $buy->location = $buy->port_name = $buy->address = '-hidden value-';
+
+      foreach($order->users as $orderUser)
+        if($orderUser->id === $user->id) return true;
+    } else {
+      foreach($user->roles() as $r){
+        foreach($r->privileges() as $p){
+          if($p->menu === 'order.approval'){
+            foreach($order->sells as &$sell)
+              $sell->seller = $sell->location = $sell->port_name = $sell->address = '-hidden value-';
+
+            foreach($order->buys as &$buy)
+              $buy->buyer = $buy->location = $buy->port_name = $buy->address = '-hidden value-';
+
+            return true;
+          }
+        }
+      }
+
     }
+
     return $user->id === $order->user_id;
   }
 
