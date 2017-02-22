4169184f54c5f807931f3fdac2c351c94c6fff0a
Pras Gema <prasetyo.gema@gmail.com>
2017-02-22 20:46:58 +0700

    reset seeder into correct thing

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,22 +14,25 @@ class DatabaseSeeder extends Seeder
     {
     	Model::unguard();
 
-        $this->call(UsersTableSeeder::class);
-        $this->call(CompaniesTableSeeder::class);
-        // $this->call(OrderApprovalSchemesTableSeeder::class);
-        $this->call(CountriesTableSeeder::class);
-        // $this->call(ConcessionTableSeeder::class);
-        // $this->call(ProductTableSeeder::class);
-        $this->call(PortTableSeeder::class);
-        // $this->call(FactoryTableSeeder::class);
-        // $this->call(IndexTableSeeder::class);
-        // $this->call(LeadsTableSeeder::class);
-        // $this->call(MiningLicenseTableSeeder::class);
-        // $this->call(SpatialDataTableSeeder::class);
-        // $this->call(SalesTargetTableSeeder::class);
-        // $this->call(ContractsTableSeeder::class);
-        // $this->call(ShipmentTableSeeder::class);
-        // $this->call(DocumentTableSeeder::class);
+        if(config('app.deployment') == 'berau' && config('app.env') == 'production') $this->call(ProductionBerauSeeder::class);
+        else {
+            $this->call(UsersTableSeeder::class);
+            $this->call(CompaniesTableSeeder::class);
+            $this->call(OrderApprovalSchemesTableSeeder::class);
+            $this->call(CountriesTableSeeder::class);
+            $this->call(ConcessionTableSeeder::class);
+            $this->call(ProductTableSeeder::class);
+            $this->call(PortTableSeeder::class);
+            $this->call(FactoryTableSeeder::class);
+            $this->call(IndexTableSeeder::class);
+            $this->call(LeadsTableSeeder::class);
+            $this->call(MiningLicenseTableSeeder::class);
+            $this->call(SpatialDataTableSeeder::class);
+            $this->call(SalesTargetTableSeeder::class);
+            $this->call(ContractsTableSeeder::class);
+            $this->call(ShipmentTableSeeder::class);
+            $this->call(DocumentTableSeeder::class);
+        }
 
         Model::reguard();
     }
--- database/seeds/ProductionBerauSeeder.php
+++ database/seeds/ProductionBerauSeeder.php
@@ -20,11 +20,11 @@ class ProductionBerauSeeder extends Seeder
   	 */
 
     $users = [
-        [ 'id' => 1, 'name' => 'Prasetyo Nugraha Gema', 'email' => 'pras@volantech.io', 'title' => 'Admin', 'image' => './images/default.png', 'phone'=> '+6281517051234', 'employee_no'=>'VTI1', 'manager_id'=>null],
-        [ 'id' => 2, 'name' => 'Yudhi Saputra Intan', 'email' => 'yudhi.saputra@beraucoal.co.id', 'title' => 'Marketing & Sales Adm. Superintendence', 'image' => './images/default.png', 'phone'=> '1234567890', 'employee_no'=>'EMP-123', 'manager_id'=>4],
-        [ 'id' => 3, 'name' => 'Rory Surya Perdana', 'email' => 'rori@beraucoal.co.id', 'title' => 'Marketing Researcher Sr. Specialist', 'image' => './images/default.png', 'phone'=> '1234567890', 'employee_no'=>'EMP-123', 'manager_id'=>4],
-        [ 'id' => 4, 'name' => 'Supandri Yaman', 'email' => 'andri@beraucoal.co.id', 'title' => 'Dept. Head Marketing & Sales Adm. Supp.', 'image' => './images/default.png', 'phone'=> '1234567890', 'employee_no'=>'EMP-123', 'manager_id'=>null],
-        [ 'id' => 5, 'name' => 'Andry Octavianus', 'email' => 'andri.o@beraucoal.co.id', 'title' => 'Sales Specialist Area 3', 'image' => './images/default.png', 'phone'=> '1234567890', 'employee_no'=>'EMP-123', 'manager_id'=>null],
+    	[ 'id' => 1, 'name' => 'Prasetyo Nugraha Gema', 'email' => 'pras@volantech.io', 'title' => 'Admin', 'image' => './images/default.png', 'phone'=> '+6281517051234', 'employee_no'=>'VTI1', 'manager_id'=>null],
+      [ 'id' => 2, 'name' => 'Yudhi Saputra Intan', 'email' => 'yudhi.saputra@beraucoal.co.id', 'title' => 'Marketing & Sales Adm. Superintendence', 'image' => './images/default.png', 'phone'=> '1234567890', 'employee_no'=>'EMP-123', 'manager_id'=>4],
+      [ 'id' => 3, 'name' => 'Rory Surya Perdana', 'email' => 'rori@beraucoal.co.id', 'title' => 'Marketing Researcher Sr. Specialist', 'image' => './images/default.png', 'phone'=> '1234567890', 'employee_no'=>'EMP-123', 'manager_id'=>4],
+      [ 'id' => 4, 'name' => 'Supandri Yaman', 'email' => 'andri@beraucoal.co.id', 'title' => 'Dept. Head Marketing & Sales Adm. Supp.', 'image' => './images/default.png', 'phone'=> '1234567890', 'employee_no'=>'EMP-123', 'manager_id'=>null],
+      [ 'id' => 5, 'name' => 'Andry Octavianus', 'email' => 'andri.o@beraucoal.co.id', 'title' => 'Sales Specialist Area 3', 'image' => './images/default.png', 'phone'=> '1234567890', 'employee_no'=>'EMP-123', 'manager_id'=>null],
     ];
 
     foreach($users as $user){
@@ -89,5 +89,53 @@ class ProductionBerauSeeder extends Seeder
   	 * COMPANY SEEDER
   	 */
 
+    Area::create(['id' => 1, 'area_name' => 'Area 1', 'description' => 'Area 1', 'status' => 'a']);
+    Area::create(['id' => 2, 'area_name' => 'Area 2', 'description' => 'Area 2', 'status' => 'a']);
+    Area::create(['id' => 3, 'area_name' => 'Area 3', 'description' => 'Area 3', 'status' => 'a']);
+    Area::create(['id' => 4, 'area_name' => 'Area 4', 'description' => 'Area 4', 'status' => 'a']);
+
+    Company::create([
+      'company_name' => 'Berau Coal, PT',
+      'user_id' => '1',
+      'area_id' =>  2,
+      'phone' => '021 29669700',
+      'is_affiliated'=>'1',
+      'annual_sales'=>'0',
+      'preferred_trading_term'=>'FOB MV',
+      'preferred_payment_term'=> 'LC',
+      'purchasing_countries'=>'',
+      'email' => '',
+      'web' => '',
+      'country'=> 'ID',
+      'industry' => 'Coal Mining',
+      'city' => 'Kalimantan Timur',
+      'address' =>'Menara Sunlife 17th Floor, Jl. Dr. Ide Anak Agung Gede Blok 63, South Jakarta 12950',
+      'description' => '',
+      'company_type' => 's',
+      'purchasing_countries' => 'ID',
+      'status' => 'a'
+    ]);
+
+
+  	/*
+  	 * PORT SEEDER
+  	 */
+
+  	$model = Port::create([
+      'port_name' => 'Berau Coal, PT', 'owner' => 'Kalimantan Timur', 'is_private' => 1, 'location' => 'Sungai Segah', 'size' => 0, 'river_capacity' => 0, 'latitude' => 2.156904, 'longitude' => 117.4911258, 'anchorage_distance' => 0, 'has_conveyor' => 0, 'has_crusher' => 0, 'has_blending' => 0, 'draft_height' => 0, 'river' => 'Riv', 'open_sea_distance' => 20,
+      'daily_discharge_rate' => $faker->numberBetween($min = 1, $max = 55), 'status' => 'a'
+    ]);
+    if(isset($port['company'])) $model->companies()->attach(1);
+
+    /*
+     * INDEX SEEDER
+     */
+
+    Index::create(['id'=>1, 'index_provider' => 'HBA', 'index_name' => 'GAR 6322', 'quality' => 'GAR 6322', 'frequency'=>'d']);
+		Index::create(['id'=>2, 'index_provider' => 'HBA*', 'index_name' => 'GAR 6322', 'quality' => 'GAR 6322', 'frequency'=>'d']);
+		Index::create(['id'=>3, 'index_provider' => 'GCI NEW', 'index_name' => 'GAR 6322', 'quality' => 'GAR 6322', 'frequency'=>'d']);
+		Index::create(['id'=>4, 'index_provider' => 'Coalfax Index', 'index_name' => 'GAR 6322', 'quality' => 'GAR 6322', 'frequency'=>'d']);
+		Index::create(['id'=>5, 'index_provider' => 'ICI 1 Index', 'index_name' => 'GAR 6322', 'quality' => 'GAR 6322', 'frequency'=>'d']);
+		Index::create(['id'=>6, 'index_provider' => 'Platts (FOB Kalimantan)', 'index_name' => 'GAR 6322', 'quality' => 'GAR 6322', 'frequency'=>'d']);
   }
 }
