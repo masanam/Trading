eeb6de1f2b361ab6a48ec179546ffb9f309d111e
martin s <philipus.martin@rocketmail.com>
2017-02-23 15:38:43 +0700

    benerin bug leads

--- public/angular/order/controllers/add-leads.modal.controller.js
+++ public/angular/order/controllers/add-leads.modal.controller.js
@@ -18,25 +18,29 @@ angular.module('order').controller('AddLeadsModalController', ['$uibModalInstanc
     $scope.currencies = Currency.currencies;
 
     $scope.changeLead = function(lead){
-      $scope.selected.item = lead;
-      if(!$scope.selected.item.pivot){
-        $scope.selected.item.pivot = {};
+      if(lead){
+        $scope.selected.item = lead;
+        if(!$scope.selected.item.pivot){
+          $scope.selected.item.pivot = {};
+        }
+        $scope.selected.item.pivot.base_currency_id = $scope.defaultCurrency;
+        $scope.selected.item.pivot.deal_currency_id = $scope.selected.item.currency;
+        $scope.selected.item.pivot.volume = $scope.selected.item.volume - $scope.getUsed($scope.selected.item);
+        $scope.selected.item.pivot.trading_term = $scope.selected.item.trading_term;
+        $scope.selected.item.pivot.payment_term = $scope.selected.item.payment_term;
+        $scope.selected.item.pivot.price = $scope.selected.item.price;
+        $scope.selected.item.pivot.deal_price = $scope.selected.item.price;
+        $scope.selected.item.pivot.exchange_rate = 1;
+        $scope.findExchange_rate($scope.selected.item.currency);
       }
-      $scope.selected.item.pivot.base_currency_id = $scope.defaultCurrency;
-      $scope.selected.item.pivot.deal_currency_id = $scope.selected.item.currency;
-      $scope.selected.item.pivot.volume = $scope.selected.item.volume - $scope.getUsed($scope.selected.item);
-      $scope.selected.item.pivot.trading_term = $scope.selected.item.trading_term;
-      $scope.selected.item.pivot.payment_term = $scope.selected.item.payment_term;
-      $scope.selected.item.pivot.price = $scope.selected.item.price;
-      $scope.selected.item.pivot.deal_price = $scope.selected.item.price;
-      $scope.selected.item.pivot.exchange_rate = 1;
-      $scope.findExchange_rate($scope.selected.item.currency);
     };
 
     $scope.getUsed = function(lead){
       $scope.used = 0;
-      for(var i = 0; i < lead.used.length; i++){
-        $scope.used += lead.used[i].volume;
+      if(lead.used){
+        for(var i = 0; i < lead.used.length; i++){
+          $scope.used += lead.used[i].volume;
+        }
       }
       return $scope.used;
     };
